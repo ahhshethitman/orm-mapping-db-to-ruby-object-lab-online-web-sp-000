f7553d4824ad10495430fbb72b7b3299ffdba245
Hunter Sides <hunter.s.1337@gmail.com>
2020-08-13 20:11:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -89,6 +89,16 @@ class Student
   end
   
   def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    ORDER BY id
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
   def self.all_students_in_grade_X
